36dfeb15130eec34a76417b753252f06c5ce9814
Mart van de Ven <m@type.hk>
2025-12-19 19:24:37 +0800

    merge

--- setup.sh
+++ setup.sh
@@ -25,9 +25,14 @@ fi
 # The directory where this script is located.
 PROJECT_ROOT="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_DEST_DIR="$HOME/.config/chezetc"
+ETC_SRC="$HOME/.local/share/chezetc"
+ETC_DST="/etc"
+ETC_CFG="$HOME/.config/chezetc/chezetc.toml"
 
 echo "Generating chezetc.toml from template..."
+echo $PROJECT_ROOT
+echo $ETC_CFG  
 mkdir -p "$CONFIG_DEST_DIR"
-$HOME/.tools/chezetc/chezetc execute-template < "$PROJECT_ROOT/chezetc.toml" > "$CONFIG_DEST_DIR/chezetc.toml"
+$HOME/.tools/chezetc/chezetc execute-template < "$PROJECT_ROOT/chezetc.toml" > $ETC_CFG
 
 echo "Successfully installed chezetc.toml to $CONFIG_DEST_DIR"
